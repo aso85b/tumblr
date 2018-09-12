c058d82f68d60db0adc4948a466deb3aeab52d7d
Aso <aso85b@gmail.com>
2018-09-12 13:37:19 +0430

    Add comment to post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.create(params[:comment].permit(:name,:body))
+
+      redirect_to post_path(@post)
+    end
+
+    def update
+    end
+
+    def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to post_path(@post)
+    end
+
+end
